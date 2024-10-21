a2575a668857641531ca55c476ff6da0d2661f35
xvekii <vedran.dautanec@gmail.com>
2024-10-21 20:36:12 +0200

    Implement first number tracki with temp object

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const inputs = {
   secondNumber: null,
 }
 
+const temp = {
+  num1Arr: [],
+  num2Arr: [], 
+}
+
+
 function add(firstNum, secondNum) {
   return firstNum + secondNum;
 }
@@ -30,9 +36,22 @@ function operate(operator, firstNum, secondNum) {
 btns.forEach(function(e) {
   e.addEventListener("click", (event) => {
     // If class of the button is a number, then store in a variable
-    // Before clicking the operator keep concat numbers - function?
-    // After clicking operator, update first number
-    // After clicking = or * / etc., update second number
+    if (e.classList.contains("num")) {
+      // Concat numbers into array else block function btns
+      // Before clicking the operator keep concat numbers - function?
+      
+      temp.num1Arr.push(event.target.textContent);
+      console.log(temp.num1Arr);
+      
+      // If operator is clicked, store into inputs.firstNumber
+      // After clicking operator, update first number
+      // After clicking = or * / etc., store second number
+    } else if (e.classList.contains("funct-btn")) {
+      inputs.firstNumber = Number(temp.num1Arr.join(""));
+      console.log(inputs.firstNumber);
+      console.log(typeof inputs.firstNumber);
+    }
+    
 
     // inputs.firstNumber = event.target.textContent;
     // console.log(inputs.firstNumber);
