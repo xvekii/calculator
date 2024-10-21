ed60c76a49e339ff929e4486b2d7d113018f3aec
xvekii <vedran.dautanec@gmail.com>
2024-10-21 18:54:41 +0200

    Add btns eventListener

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const btns = document.querySelectorAll("button");
 
 const inputs = {
   operator: null,
@@ -24,3 +25,17 @@ function divide(firstNum, secondNum) {
 function operate(operator, firstNum, secondNum) {  
   // Calls one of the above functions on the numbers
 }
+
+
+btns.forEach(function(e) {
+  e.addEventListener("click", (event) => {
+    // If class of the button is a number, then store in a variable
+    // Before clicking the operator keep concat numbers - function?
+    // After clicking operator, update first number
+    // After clicking = or * / etc., update second number
+
+    // inputs.firstNumber = event.target.textContent;
+    // console.log(inputs.firstNumber);
+    // console.log(inputs);
+  });
+});
