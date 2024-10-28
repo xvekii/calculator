d6ca9ebdb0b41c1ffdb74def6562a02e7ff27f56
xvekii <vedran.dautanec@gmail.com>
2024-10-28 10:42:20 +0100

    Refactor the code to use event delegation on btnsMainCont

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btns = document.querySelectorAll("button");
+const btnsMainCont = document.querySelector(".buttons-main-container");
 const funcBtns = document.querySelectorAll(".funct-btn");
 let resultSpan = document.querySelector(".result-span");
 const decimalBtn = document.querySelector(".decimal");
@@ -73,51 +74,51 @@ function toggleFuncBtns(enable) {
   });
 }
 
-btns.forEach(function(e) {
-  e.addEventListener("click", (event) => {
-    // If class of the button is a number, then store in a variable
-    if (e.classList.contains("num")) {
-      toggleFuncBtns(true);
-      
-      // Temporarily store first number digits and show them on the screen
-      temp.num1Arr.push(event.target.textContent);
 
-      // Make sure . can't be used twice in a number
-      if (event.target.textContent === ".") {
-        decimalBtn.disabled = true;
-      }
+btnsMainCont.addEventListener("click", (event) => {
+  // If class of the button is a number, then store in a variable
+  if (event.target.classList.contains("num")) {
+    toggleFuncBtns(true);
+    
+    // Temporarily store first number digits and show them on the screen
+    temp.num1Arr.push(event.target.textContent);
 
-      if (temp.num1Arr[0] === ".") {
-        temp.num1Arr.unshift("0");
-        decimalBtn.disabled = true;
-      }
-      
-      if (temp.num1Arr[0] === "0" && temp.num1Arr[1] === "0") {
-        temp.num1Arr.pop();
-      }
-
-      if (temp.num1Arr.length == 2 && temp.num1Arr[0] === "0") {
-        temp.num1Arr.shift();
-      }
-
-      resultSpan.textContent = temp.num1Arr.join("");
-      console.log(temp.num1Arr);
-      
-      // If operator is clicked, store into inputs.firstNumber
-      // After clicking operator, update first number
-      // After clicking = or * / etc., store second number
-    } else if (e.classList.contains("funct-btn") && funcBtnsEnabled == true) {
-      inputs.operator = event.target.textContent;
-      inputs.firstNumber = +temp.num1Arr.join("");
-      temp.num1Arr = [];
-      
-      resultSpan.textContent = inputs.firstNumber + inputs.operator;
-      funcBtnsEnabled = false;
-      
-      console.log(event.target.textContent);
+    // Make sure . can't be used twice in a number
+    if (event.target.textContent === ".") {
+      decimalBtn.disabled = true;
     }
 
-  if (e.classList.contains("num") && inputs.firstNumber !== null) {
+    if (temp.num1Arr[0] === ".") {
+      temp.num1Arr.unshift("0");
+      decimalBtn.disabled = true;
+    }
+    
+    if (temp.num1Arr[0] === "0" && temp.num1Arr[1] === "0") {
+      temp.num1Arr.pop();
+    }
+
+    if (temp.num1Arr.length == 2 && temp.num1Arr[0] === "0") {
+      temp.num1Arr.shift();
+    }
+
+    resultSpan.textContent = temp.num1Arr.join("");
+    console.log(temp.num1Arr);
+    
+    // If operator is clicked, store into inputs.firstNumber
+    // After clicking operator, update first number
+    // After clicking = or * / etc., store second number
+  } else if (event.target.classList.contains("funct-btn") && funcBtnsEnabled == true) {
+    inputs.operator = event.target.textContent;
+    inputs.firstNumber = +temp.num1Arr.join("");
+    temp.num1Arr = [];
+    
+    resultSpan.textContent = inputs.firstNumber + inputs.operator;
+    funcBtnsEnabled = false;
+    
+    console.log(event.target.textContent);
+  }
+
+  if (event.target.classList.contains("num") && inputs.firstNumber !== null) {
       temp.num2Arr.push(event.target.textContent);
       resultSpan.textContent = inputs.firstNumber + inputs.operator + temp.num2Arr.join("");
 
@@ -129,9 +130,5 @@ btns.forEach(function(e) {
       // if (inputs.operator === "+") {
       //   add(inputs.firstNumber, inputs.secondNumber);
       // }
-  }
-
-  
-    
+  } 
   });
-});
